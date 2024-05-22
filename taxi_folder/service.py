de158431e50f1eeaa3627037f7261d9caaba7a8f
aaxelf <iakselrod22@gmail.com>
2024-05-23 01:09:30 +0300

    добавила регистрацию и вход

--- taxi_folder/service.py
+++ taxi_folder/service.py
@@ -1,14 +1,27 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, flash, session
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
+from flask_login import UserMixin, LoginManager, login_user, login_required, logout_user, current_user
 
-service = Flask(__name__)  # у сервиса имя - имя этого файла
 
-
-service = Flask(__name__)  # у сервиса имя - имя этого файла
-service.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///order-history.db'  # подключаем базу данных
+service = Flask(__name__)
+service.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///order-history.db'
 service.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+service.config['SECRET_KEY'] = 'supersecretkey'  # ключ для управления сессией
+
 db = SQLAlchemy(service)
+
+login_manager = LoginManager()
+login_manager.init_app(service)
+login_manager.login_view = 'login'
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), nullable=False, unique=True)
+    email = db.Column(db.String(150), nullable=False, unique=True)
+    password = db.Column(db.String(150), nullable=False)
 
 
 class Taxi_order(db.Model):
@@ -18,32 +31,85 @@ class Taxi_order(db.Model):
     finish = db.Column(db.String(300), nullable=False)
     # макс длины 300, нельзя поле оставлять пустым(nullable)
     date = db.Column(db.DateTime, default=datetime.utcnow)  # по умолчанию время создания записи
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
         return '<Taxi_order %r>' % self.id  # при выборе
         # объекта будет выдаваться этот объект и его ид
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+@service.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        hashed_password = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
+
+        new_user = User(username=username, email=email, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        login_user(new_user)
+        return redirect(url_for('index'))
+
+    return render_template('register.html')
+
+
+@service.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user = User.query.filter_by(email=email).first()
+        if user and check_password_hash(user.password, password):  # проверка на правильность введённых данных
+            login_user(user)
+            return redirect(url_for('index'))
+        else:
+            flash('При входе произошла ошибка. Проверьте введённые данные.', 'danger')
+
+    return render_template('login.html')
+
+
+@service.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+@service.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html', name=current_user.username)
+
+
 @service.route('/create-order', methods=['POST', 'GET'])
+@login_required
 def create_order():
     if request.method == "POST":
         start = request.form["start"]
         finish = request.form["finish"]
 
-        taxi_order = Taxi_order(start=start, finish=finish)
+        taxi_order = Taxi_order(start=start, finish=finish, user_id=current_user.id)
         try:
             db.session.add(taxi_order)
             db.session.commit()
             return redirect('/order-history')
         except:
-            return "При создании заказа произошла ошибка"
+            return "При создании заказа произошла ошибка."
     else:
         return render_template("create-order.html")
 
 
 @service.route('/order-history')  # отображение данных из бд
+@login_required
 def orders():
-    taxi_orders = Taxi_order.query.order_by(Taxi_order.date.desc()).all()  # query даёт обратиться к бд
+    taxi_orders = Taxi_order.query.filter_by(user_id=current_user.id).order_by(Taxi_order.date.desc()).all()  # query даёт обратиться к бд
     # заказов, сортировка по дате, first - отображение только первой записи, все - all, desc - descending
     return render_template("order-history.html", taxi_orders=taxi_orders)
 
@@ -60,5 +126,7 @@ def about():
 
 
 if __name__ == "__main__":  # если ЭТО основной файл для запуска
+    with service.app_context():
+        db.create_all()
     service.run(debug=True)  # true чтобы ошибки вылезали прямо на страничке, потом
     # надо будет поменять на false, чтобы конечному пользователю они не были видны
